e6cd10c8358b33f5318361503d49700773bfffe6
Deep Kansara <deepkansara1212@gmail.com>
2025-04-10 10:42:41 +0530

    Updated

---
+++ Chapter 8/04_Practice.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+    char str1[] = {'H', 'e', 'l', 'l', 'o', '\0', 'W', 'o', 'r', 'l', 'd', '!', '\0'};
+    char str2[] = "Hello";
+                   Ifmmp
+
+    str[i] = str[i] + 1
+
+    printf("%s\n", str1);
+    printf("%s\n", str2);
+
+
+    return 0;
+}
